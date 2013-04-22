1e7d675e1b1dd594ed13c4ea2c57919e40462ac1
Maruf Hasan Bulbul <maruf.freelancer@gmail.com>
2013-04-22 18:28:21 +0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Campuswise::Application.config.secret_token = '81d93f0f197d5a21d7f20c85444c42a64a556f7ca3d604e7b3934fa355cb9b7ad35140aebfba96ce3e1b74ea147fdc9855bd1f72480429009eb54a6cea396451'
