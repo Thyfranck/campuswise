4bd27b419bfab6cfac6bd74f2c33b16be5e52476
Maruf Hasan Bulbul <maruf.freelancer@gmail.com>
2013-04-29 02:51:16 +0600

    school images

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -12,6 +12,12 @@ class StaticController < ApplicationController
 
   def public_find_books
     @school = School.find(params[:school_id])
+    @school_images = @school.school_images.order("created_at desc")
+    
+    if @school_images.count < 2
+      @school_images = SchoolImage.order("created_at desc").limit(5)
+    end
+
     respond_to do |format|
       if @school.present?
         session[:school_id] = @school.id
