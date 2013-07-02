5749f6580f0b61fef2ca876f6b2a2536b06fb263
Maruf Hasan Bulbul <maruf.freelancer@gmail.com>
2013-07-02 19:11:06 +0600

    Fixed stripe credentials

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,8 +3,8 @@ if Rails.env.development?
   STRIPE_PUBLIC_KEY = "pk_test_RIAHJE3jDPu7SKcnQUr2rJu9"  # test account
 elsif Rails.env.staging?
   Stripe.api_key = "sk_test_W3fW7v8oI9NJrviXc1jV58xw"     # test account
-  STRIPE_PUBLIC_KEY = "sk_test_W3fW7v8oI9NJrviXc1jV58xw"  # test account
+  STRIPE_PUBLIC_KEY = "pk_test_RIAHJE3jDPu7SKcnQUr2rJu9"  # test account
 elsif Rails.env.production?
-  Stripe.api_key = "sk_live_jQ0IlPMXQJ4z0WiFK8ARKa4y"     # Live account
-  STRIPE_PUBLIC_KEY = "pk_live_sAkQxoL9kpnbEBNwxQPDQYMR"  # Live account
+  Stripe.api_key = "sk_live_rWQ9xzfTAwHl5wUIEE1KbvBr"     # Live account
+  STRIPE_PUBLIC_KEY = "pk_live_v8C3f4k2mQrGRUOAqJkKMqg9"  # Live account
 end
