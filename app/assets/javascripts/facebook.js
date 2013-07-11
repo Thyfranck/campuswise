c92202155760b6b8ce1a8a9a2897f3c0e3ab7034
Mazharul Islam <mithu.cste.30@gmail.com>
2013-07-11 14:00:17 +0600

    facebook_app

--- app/assets/javascripts/facebook.js
+++ app/assets/javascripts/facebook.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     $.getScript('//connect.facebook.net/en_UK/all.js', function(){
         window.fbAsyncInit = function() {
             FB.init({
-                appId: '362079183898081'
+                appId: '408988775883174'
             });
         };
     });
